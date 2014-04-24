e6e4e5a6515d501ce80083b700516627931b77d3
NatBV <nathalie.vicente@gmail.com>
2014-04-24 09:12:29 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  - Programming Assignment 2 - Peer Assessment - 
+## This work consists of two functions (makeCacheMatrix and CacheSolve)
+## that enable the inverse of a matrix to be cached instead of being
+## calculated every time it is required (which is specially useful when
+## large matrices are used and/or when having to compute it repeatedly).
+## [by Nathalie Vicente - Apr 2014]
 
-## Write a short comment describing this function
 
+## makeCacheMatrix creates a list of four functions (set, get, setinv,
+## getinv) that act together like a special matrix, allowing the inverse
+## of a matrix to be cached
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## The cached inverse of x, which initially is set to NULL
+    i <- NULL
+    
+    ## Function used to store/set a new matrix x
+    set <- function(y)
+    {
+        x <<- y
+        i <<- NULL
+    }
+    
+    ## Function used to retrieve the stored matrix x
+    get <- function() x
+    
+    ## Function used to store/set the inverse of x
+    setinv <- function(inv) i <<- inv
+    
+    ## Function used to retrieve the stored inverse of x
+    getinv <- function() i
+    
+    ## Return a list containing the four functions
+    list(set = set,
+         get = get,
+         setinv = setinv,
+         getinv = getinv)  
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve calculates/retrieves the cached value of the inverse of
+## a matrix using makeCacheMatrix; it will calculate it for the first time
+## and whenever a new matrix is set, otherwise it will retrieve the cached
+## value
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Retrieve the cached value of the inverse of x
+    invm <- x$getinv()
+    
+    ## If it is the first time the inverse is required or a new matrix has
+    ## been set (inv == NULL), the inverse will be computed
+    if(is.null(invm))
+    {
+        message("MSG: Calculating data")
+        data <- x$get()
+        invm <- solve(data, ...)
+        x$setinv(invm)
+    }
+    else
+    {
+        message("MSG: Getting cached data")
+    }
+    
+    ## Return a matrix that is the inverse of x
+    invm
 }
